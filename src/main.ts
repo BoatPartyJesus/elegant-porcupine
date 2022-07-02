1be0a5174a45ec16330d4a93286e27badc3bdf31
Callum Lyall <callum.lyall@stikworx.co.uk>
2022-07-02 19:02:31 +0100

    feat: :sparkles: implement helmet library

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
 
   const options = new DocumentBuilder()
     .setTitle('RotaCloud Test Api')
